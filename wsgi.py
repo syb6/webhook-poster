2ed941d3f64fab70d32d0a8fc2218176ad6b218f
syb6 <mysidtuaha.com>
2026-09-19 05:16:57 +0500

    Updated features

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+if project_dir not in sys.path:
+    sys.path.insert(0, project_dir)
+
+from app import app as application
